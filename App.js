2d75ea73efc513adc57d05d68a247a645719e44d
Sneha Chaudhary <itschaudhary18@gmail.com>
2025-01-04 13:42:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import Resturant from "./component/basics/Resturant";
+// import Temp from "./component/Weather/Temp";
+import Todo from "./component/Todo/Todo";
+const App=()=>{
+  // return < />;
+  return < Todo/>;
+
+};
+export default App;
